435a066a08f8919fb0ce605acb65aa0c7f1add65
Wahyu Setiawan Usman <wahyusman10@gmail.com>
2023-12-01 23:07:02 +0800

    initialization of the project

---
+++ src/app/components/Search/index.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <>
+            <h1>Search Component</h1>
+            <p>Search for anime here.</p>
+        </>
+    )
+}
+
+export default Search
---
+++ src/app/search/page.js
@@ -0,0 +1,10 @@
+const SearchPage = () => {
+    return (
+        <>
+            <h1>Search Page</h1>
+            <p>Search for anime here.</p>
+        </>
+    )
+}
+
+export default SearchPage
